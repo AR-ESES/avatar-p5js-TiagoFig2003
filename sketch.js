203fe8a2102c1b84239adc87aa10ed845227da5b
unknown <tfig2003@gmail.com>
2024-03-07 14:52:32 +0000

    avatar

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(1920, 1080);
 }
-
-function draw() {}
+function draw() {
+  background(135, 206, 235);
+  fill(19,109,21);
+  square(0, 867, 1920);
+  fill(255,226,174);
+  circle(960, 520, 100);
+  fill(255);
+  circle(975, 517, 30);
+  fill(255);
+  circle(945, 517, 30);
+  fill(51);
+  circle(945, 517, 10);
+  fill(51);
+  circle(975, 517, 10);
+  fill(51);
+  rect(927, 708, 35, 160, 25);
+  fill(51);
+  rect(963, 708, 35, 160, 25);
+  fill(255,51,51);
+  rect(915, 570, 90, 150, 25);
+  rect(895, 570, 30, 160, 25);
+  fill(255,51,51);
+  rect(995, 570, 90, 30, 25);
+  fill(255,51,51);
+  rect(1055, 520, 30, 70, 25);
+}
